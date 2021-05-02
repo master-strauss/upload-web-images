6487bea86300b164b731544da79005a364d1df91
Patrick <48039496+master-strauss@users.noreply.github.com>
2021-05-02 12:09:36 +0100

    Update hello-from-lambda.js

--- src/handlers/hello-from-lambda.js
+++ src/handlers/hello-from-lambda.js
@@ -1,13 +1,39 @@
-/**
- * A Lambda function that returns a string.
- */
-exports.helloFromLambdaHandler = async () => {
-    // If you change this message, you will need to adjust tests in hello-from-lambda.test.js
-    const message = 'Hello from Lambda!';
+'use strict'
 
-    // All log statements are written to CloudWatch by default. For more information, see
-    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-    console.log(message);
+const AWS = require('aws-sdk')
+AWS.config.update({ region: process.env.AWS_REGION || 'eu-west-1' })
+const s3 = new AWS.S3()
 
-    return message;
-};
+// Main Lambda entry point
+exports.handler = async (event) => {
+  const result = await getUploadURL()
+  console.log('Result: ', result)
+  return result
+}
+
+const getUploadURL = async function() {
+  const actionId = parseInt(Math.random()*10000000)
+  
+  const s3Params = {
+    Bucket: process.env.UploadBucket,
+    Key:  `${actionId}.jpg`,
+    ContentType: 'image/jpeg' // Update to match whichever content type you need to upload
+    //ACL: 'public-read'      // Enable this setting to make the object publicly readable - only works if the bucket can support public objects
+  }
+
+  console.log('getUploadURL: ', s3Params)
+  return new Promise((resolve, reject) => {
+    // Get signed URL
+    resolve({
+      "statusCode": 200,
+      "isBase64Encoded": false,
+      "headers": {
+        "Access-Control-Allow-Origin": "*"
+      },
+      "body": JSON.stringify({
+          "uploadURL": s3.getSignedUrl('putObject', s3Params),
+          "photoFilename": `${actionId}.jpg`
+      })
+    })
+  })
+}
